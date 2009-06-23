b7a2986b5805faafc8d64420797394a3da7952b3
Jonathan Hoyt <jonmagic@Nikita.local>
2009-06-23 09:06:51 -0400

    added in missing directories

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+  # Scrub sensitive parameters from your log
+  # filter_parameter_logging :password
+  
+  before_filter :http_basic_authenticate
+  
+  protected
+    
+    def http_basic_authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == APP_CONFIG[:username] && password == APP_CONFIG[:password]
+      end
+    end
+  
+end
