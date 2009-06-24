76e34ad5e4b7eb8df65caeb469d736bacd570654
Jonathan Hoyt <jonmagic@Nikita.local>
2009-06-24 08:57:31 -0400

    app is all prepped, time to start some development

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   
+  map.resources :sample
+  
+  map.root :controller => 'sample', :action => 'index'
+  
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
