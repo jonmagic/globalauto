cfd2902192354c71408867c0ec14e4652187f81d
Jonathan Hoyt <jonmagic@Nikita.local>
2009-07-07 09:57:37 -0400

    fixed report so it only shows recorded entries

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -5,20 +5,26 @@ class Admin::ReportsController < ApplicationController
   end
   
   def recorded_vs_flatrate
+    # start_date and end_date stuff
     params[:start_date]     ||= LastDayNextDay.last('friday')
     params[:end_date]       ||= LastDayNextDay.next('thursday')
-    if params[:technician_id] then @selected = params[:technician_id] end
-    if params[:technician_id] == 'all' then params[:technician_id] = nil end
-    params[:technician_id]  ||= (0..1000)
-    
     start_date = params[:start_date].to_s + " 00:00:00"
     end_date = params[:end_date].to_s + " 23:59:59"
-    
-    @jobs = Job.all(:conditions => {:completed => start_date..end_date, :technician_id => params[:technician_id]})
-    @runningdiff = []
-    
     @start_date = params[:start_date]
     @end_date = params[:end_date]
+    
+    # getting/setting technician_id
+    if params[:technician_id] then @selected = params[:technician_id] end
+    if params[:technician_id] == 'all' then params[:technician_id] = nil end
+    params[:technician_id]  ||= (0..1000)    
+    
+    # make it so we only find jobs with flatrate and extra time recorded
+    flatrate_range = (0..99999999)
+    
+    @jobs = Job.all(:conditions => {:completed => start_date..end_date, :technician_id => params[:technician_id], :flatrate_time => flatrate_range})
+
+    # used to create a running differential
+    @runningdiff = []
   end
   
 end
