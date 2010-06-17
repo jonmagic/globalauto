037bbf86505443c76e92e95a35f209349302d341
Jonathan Hoyt <jonmagic@gmail.com>
2010-06-17 10:07:37 -0400

    started implementing week calendar but then realized it won't work for what I need

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,17 +1,10 @@
 class ScheduleController < ApplicationController
 
   def index
-    redirect_to "/schedule/#{Date.today.strftime("%m%d%Y")}"
+    
   end
   
   def show
-    @day = (params["id"][4..7]+"-"+params["id"][0..1]+"-"+params["id"][2..3]).to_date
-    @technicians = params[:technician_id] ? [Technician.find(params[:technician_id])] : Technician.all
-    @hours = []
-    ["08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", 
-    "11:30", "12:00", "12:30", "01:00", "01:30", "02:00", "02:30", 
-    "03:00", "03:30", "04:00", "04:30", "05:00", "05:30"].each do |time|
-      @hours << {:digits => time, :class => "time_#{time.split(':').join('_')}"}
-    end
+    
   end
 end
--- public/javascripts/schedule.js
+++ public/javascripts/schedule.js
@@ -1,303 +1,51 @@
-var Parse = {
-  dateTimeString: function(string){
-    var time    = string.split('T')[1].split('-')[0].split(':');
-    var hours   = time[0]; hours   = parseInt(hours, 10);   if(hours   < 10){ hours   = '0' + hours   }else{ hours   += '' };
-    var minutes = time[1]; minutes = parseInt(minutes, 10); if(minutes < 10){ minutes = '0' + minutes }else{ minutes += '' };
-    var seconds = time[2]; seconds = parseInt(seconds, 10); if(seconds < 10){ seconds = '0' + seconds }else{ seconds += '' };
-    var date  = string.split('T')[0].split('-');
-    var month = date[1];
-    var day   = date[2];
-    var year  = date[0];
-    // return {'month':month,'day':day,'year':year,'hours':hours,'minutes':minutes,'seconds':seconds};
-    return Date.parse(year+'-'+month+'-'+day+' '+hours+':'+minutes+':'+seconds)
-  }
-}
-var Jobs = {
-  getAndDraw: function(){
-    $.ajax({
-      url: window.location.href+'/jobs',
-      dataType: 'json',
-      success: function(data, result){
-        $.each(data, function(i, job){
-          Job.draw(job);
-        });
-      }
-    });
-  }
-};
-var Job = {
-  draw: function(job){
-    if(job.timers.length > 0){
-      $('#'+job.id).remove();
-      $.each(job.timers, function(){Timer.draw(this, job)});
-    }else{
-      // setup box dimensions
-      if(job.flatrate_time){
-        var difference = job.flatrate_time*60;
-      }else{
-        var difference = 60;
-      }
-      if($('div#'+job.id).length == 0){
-        var ref = 'RO# '+job.ro;
-        if (job.ro === null && job.vehicle !== null){
-          ref = job.vehicle;
-        }
-        $('body').append('<div class="job" id="'+job.id+'" data-ro="'+job.ro+'"><h2>'+job.lastname+'<span>'+ref+'</span><span class="ui-icon ui-icon-pencil"></span></h2><p>'+job.description+'</p></div>');
-      }
-      var box = $('#'+job.id);
-      var dimensions = Dimensions.calculate(job.scheduled_at, difference, job.technician_id);
-      if((dimensions.top + dimensions.height) > (BOTTOM + $('tr.time_05_30 td').height() + 4) && dimensions.top > 0){
-        dimensions.height = (BOTTOM + $('tr.time_05_30 td').height() + 4) - dimensions.top;
-      }else if(dimensions.top < 0 && (dimensions.top + 1435*pixelsPerMinute + dimensions.height) > (BOTTOM + $('tr.time_05_30 td').height() + 4)){
-        dimensions.height = (dimensions.top + 1440*pixelsPerMinute + dimensions.height) - (BOTTOM + $('tr.time_05_30 td').height() + 8);
-        if(dimensions.height < 40){
-          dimensions.height = 40;
-        };
-        dimensions.top = $('tr.time_08_00 td').offset().top;
-      }
-      console.log(job.id)
-      box.css({left: dimensions.left, top: dimensions.top, height: dimensions.height, width: $('td#'+job.technician_id).outerWidth() - 4, backgroundColor: Job.state_color(job.state)});
-    };
-  },
-  compose: function(tech_id){
-    DIALOG.dialog('close');
-    DIALOG.empty();
-    DIALOG.dialog('option', 'title', 'New Job');
-    DIALOG.dialog('option', 'buttons', {
-              "Save": function(){
-                $('#dialog form').ajaxSubmit({
-                  success: function(){
-                    Jobs.getAndDraw();
-                    DIALOG.dialog("close");
-                  }
-                });
-              }
-              , "Cancel": function(){DIALOG.dialog("close")}
-            });
-    DIALOG.load('/technicians/'+tech_id+'/jobs/new', function(){
-      DIALOG.dialog("open");
-    });
-  },
-  edit: function(job_id){
-    DIALOG.dialog('close');
-    DIALOG.empty();
-    DIALOG.dialog('option', 'buttons', {
-              "Save": function(){
-                $('#dialog form').ajaxSubmit({
-                  success: function(){
-                    Jobs.getAndDraw();
-                    DIALOG.dialog("close");
-                  }
-                });
-              }
-              , "Cancel": function(){DIALOG.dialog("close")}
-            });
-    DIALOG.load('/jobs/'+job_id+'/edit', function(){
-      DIALOG.dialog("open");
-    });
-  },
-  show: function(job_id){
-    DIALOG.dialog('close');
-    DIALOG.empty();
-    DIALOG.dialog('option', 'buttons', {});
-    DIALOG.load('/jobs/'+job_id, function(){
-      if      (DIALOG.find('table.job').attr('data-state')==='scheduled'){
-        DIALOG.dialog('option', 'buttons', {
-          "No Show": function(){
-            $.ajax({
-              url: '/jobs/'+job_id+'/no_show',
-              success: function(){
-                Jobs.getAndDraw();
-                DIALOG.dialog('close');
-              }
-            });
-          },
-          "Arrived": function(){
-            $.ajax({
-              url: '/jobs/'+job_id+'/arrived',
-              success: function(){
-                Jobs.getAndDraw();
-                DIALOG.dialog('close');
-              }
-            });
-          }
-        });
-      }else if(DIALOG.find('table.job').attr('data-state')==='here'){
-        DIALOG.dialog('option', 'buttons', {
-          "Start": function(){
-            $.ajax({
-              url: '/jobs/'+job_id+'/toggle',
-              success: function(){
-                Jobs.getAndDraw();
-                DIALOG.dialog('close');
-              }
-            });
-          }
-        });
-      }else if(DIALOG.find('table.job').attr('data-state')==='no_show'){
-        console.log('no show')
-      }else if(DIALOG.find('table.job').attr('data-state')==='in_progress'){
-        DIALOG.dialog('option', 'buttons', {
-          "Stop": function(){
-            $.ajax({
-              url: '/jobs/'+job_id+'/toggle',
-              success: function(){
-                Jobs.getAndDraw();
-                DIALOG.dialog('close');
-              }
-            });
-          },
-          "Complete": function(){
-            $.ajax({
-              url: '/jobs/'+job_id+'/complete',
-              success: function(){
-                Jobs.getAndDraw();
-                DIALOG.dialog('close');
-              }
-            });
-          }
-        });
-      }else if(DIALOG.find('table.job').attr('data-state')==='pause'){
-        DIALOG.dialog('option', 'buttons', {
-          "Start": function(){
-            $.ajax({
-              url: '/jobs/'+job_id+'/toggle',
-              success: function(){
-                Jobs.getAndDraw();
-                DIALOG.dialog('close');
-              }
-            });
-          },
-          "Complete": function(){
-            $.ajax({
-              url: '/jobs/'+job_id+'/complete',
-              success: function(){
-                Jobs.getAndDraw();
-                DIALOG.dialog('close');
-              }
-            });
-          }
-        });
-      }else if(DIALOG.find('table.job').attr('data-state')==='complete'){
-        console.log('complete')
-      }
-      DIALOG.dialog('open');
-    });
-  },
-  state_color: function(state){
-    if      (state==='scheduled'){
-      return '#EEEEEE';
-    }else if(state==='here'){
-      return '#FFFF99';
-    }else if(state==='no_show'){
-      return '#800000';
-    }else if(state==='in_progress'){
-      return '#FFCC00';
-    }else if(state==='pause'){
-      return '#FFFF99';
-    }else if(state==='complete'){
-      return '#969696';
-    }
-  }
-};
-var Timer = {
-  draw: function(timer, job){
-    if(timer.end_time){
-      var difference = (Parse.dateTimeString(timer.end_time).getTime() - Parse.dateTimeString(timer.start_time).getTime())/1000/60;
-    }else{
-      var difference = (new Date().getTime() - Parse.dateTimeString(timer.start_time).getTime())/1000/60;
-    }
-    var dimensions = Dimensions.calculate(timer.start_time, difference, job.technician_id);
-    if((dimensions.top + dimensions.height) > (BOTTOM + $('tr.time_05_30 td').height() + 4) && dimensions.top > 0){
-      dimensions.height = (BOTTOM + $('tr.time_05_30 td').height() + 4) - dimensions.top;
-    }
-    if($('div#'+timer.id).length == 0){
-      $('body').append('<div class="timer" id="'+timer.id+'" data-job-id="'+job.id+'" data-ro="'+job.ro+'"><h2>RO# '+job.ro+'<span>'+job.lastname+'</span></h2><p>'+job.description+'</p></div>');
-    }
-    var box = $('#'+timer.id);
-    box.css({left: dimensions.left, top: dimensions.top, height: dimensions.height, width: $('td#'+job.technician_id).outerWidth() - 4, backgroundColor: Job.state_color(job.state)});
-  }
-};
-var Dimensions = {
-  pixelsPerMinute: function(){
-    // calculate minute height
-    Dimensions.top();
-    Dimensions.bottom();
-    var pixelsPerMinute = (BOTTOM - TOP)/600;
-    // return these values
-    return pixelsPerMinute
-  },
-  top: function(){
-    TOP = $('table#schedule').offset().top;
-  },
-  bottom: function(){
-    BOTTOM = $('tr.time_05_30 td').offset().top;
-  },
-  calculate: function(start_time, difference, technician_id){
-    var date = $('body > table').attr('data-date');
-    var day = Date.parse(date.split(' ')[1].replace('/', '-').replace('/', '-')+' '+date.split(' ')[0]);
-    var start_time = Parse.dateTimeString(start_time);
-    var start_minutes = (start_time.getTime() - day.getTime())/1000/60;
-    var top = start_minutes*pixelsPerMinute + $('tr.time_08_00').offset().top;
-    var height = difference*pixelsPerMinute;
-    var left = $('td#'+technician_id).offset().left + 1;
-    console.log(top);
-    return {'top':top,'left':left,'height':height};
-  }
-};
-// everything that gets initiated on load
-$(function(){
-  // datepicker
-  $('input#calendar').datepicker({
-    buttonImage: '/images/icons/calendar.png',
-    buttonImageOnly: true,
-    constrainInput: true,
-    dateFormat: 'mmdd20y',
-    duration: 'fast',
-    showButtonPanel: false,
-    showOn: 'button',
-    beforeShow: function(input, inst){
-      setTimeout(function(){$('#ui-datepicker-div').css('z-index', 9999)}, 100);
-    },
-    onSelect: function(date, inst){
-      document.location.href = '/schedule/'+date;
-    }
-  });
-  // figure out pixels/minute
-  pixelsPerMinute = Dimensions.pixelsPerMinute();
-  // get & draw the jobs
-  $(window).resize(function(){
-    UNITS = Dimensions.pixelsPerMinute();
-    Jobs.getAndDraw()
-  });
-  function loop(){
-    Jobs.getAndDraw();
-    setTimeout(function(){loop()}, 30000)
-  }
-  loop();
-  // setup my dialog box
-  DIALOG = $('#dialog').dialog({
-    autoOpen: false
-    , modal: true
-    , width: 600
-  });
-  // new job
-  $('#schedule tbody td').live('click', function(){
-    TIMECLASS = $(this).parent().attr('class');
-    Job.compose($(this).attr('data-tech-id'));
-  });
-  // edit job
-  $('div.job h2').live("click", function(){
-    DIALOG.dialog('option', 'title', 'RO# '+$(this).parent().attr('data-ro'));
-    Job.edit($(this).parent().attr('id'));
-  });
-  // show job
-  $('div.job p').live("click", function(){
-    DIALOG.dialog('option', 'title', 'RO# '+$(this).parent().attr('data-ro'));
-    Job.show($(this).parent().attr('id'));
-  });
-  $('div.timer').live("click", function(){
-    DIALOG.dialog('option', 'title', 'RO# '+$(this).attr('data-ro'));
-    Job.show($(this).attr('data-job-id'));
-  });
+var year = new Date().getFullYear();
+var month = new Date().getMonth();
+var day = new Date().getDate();
+
+$(document).ready(function() {
+
+	$('#calendar').weekCalendar({
+		timeslotsPerHour: 2,
+		daysToShow: 1,
+		firstDayOfWeek: 2,
+		height: function($calendar){
+			return $(window).height();
+		},
+		eventRender : function(calEvent, $event) {
+			if(calEvent.end.getTime() < new Date().getTime()) {
+				$event.css("backgroundColor", "#aaa");
+				$event.find(".time").css({"backgroundColor": "#999", "border":"1px solid #888"});
+			}
+		},
+		eventNew : function(calEvent, $event) {
+			displayMessage("<strong>Added event</strong><br/>Start: " + calEvent.start + "<br/>End: " + calEvent.end);
+			alert("You've added a new event. You would capture this event, add the logic for creating a new event with your own fields, data and whatever backend persistence you require.");
+		},
+		eventDrop : function(calEvent, $event) {
+			displayMessage("<strong>Moved Event</strong><br/>Start: " + calEvent.start + "<br/>End: " + calEvent.end);
+		},
+		eventResize : function(calEvent, $event) {
+			displayMessage("<strong>Resized Event</strong><br/>Start: " + calEvent.start + "<br/>End: " + calEvent.end);
+		},
+		eventClick : function(calEvent, $event) {
+			displayMessage("<strong>Clicked Event</strong><br/>Start: " + calEvent.start + "<br/>End: " + calEvent.end);
+		},
+		eventMouseover : function(calEvent, $event) {
+			displayMessage("<strong>Mouseover Event</strong><br/>Start: " + calEvent.start + "<br/>End: " + calEvent.end);
+		},
+		eventMouseout : function(calEvent, $event) {
+			displayMessage("<strong>Mouseout Event</strong><br/>Start: " + calEvent.start + "<br/>End: " + calEvent.end);
+		},
+		noEvents : function() {
+			displayMessage("There are no events for this week");
+		},
+		data:"events.json"
+	});
+
+	function displayMessage(message) {
+		$("#message").html(message).fadeIn();
+	}
+
+	$("<div id=\"message\" class=\"ui-corner-all\"></div>").prependTo($("body"));
+	
 });
