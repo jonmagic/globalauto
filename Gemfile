4aed500b823b094f7b6d4e67be89e59ba723f1bf
Jonathan Hoyt <jonmagic@gmail.com>
2011-04-26 10:04:04 -0400

    Updated version of Rails and locked MongoMapper version.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source :rubygems
 
-gem 'rails', '2.3.8'
+gem 'rails', '2.3.11'
 
 gem 'sqlite3-ruby'
 gem 'mongo_mapper', '0.8.4'
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -21,9 +21,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem 'days_and_times'
-  config.gem 'mongo_mapper'
+  config.gem 'mongo_mapper', :version => '0.8.4'
   config.gem 'state_machine'
-              
+
   require 'last_day_next_day'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
